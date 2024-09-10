88286431c5cd40bed0d8308d5fafdfeba6c691ba
ZackOhLantern <92610630+ZackOhLantern@users.noreply.github.com>
2024-09-10 15:39:21 -0700

    created while loops practice
    finished

---
+++ while-loop-practice.ps1
@@ -0,0 +1,12 @@
+﻿cls
+
+while(($userinput) -ne "quit"){
+    # gather user input
+    $userInput = read-host "say yes if you like scripting (enter 'quit' to stop the loop)"
+
+    #evaluate user input
+    if($userinput -eq "yes") { echo "I also love scripting! that's cool" }
+    elseif($userinput -eq "no") { echo "I really hate scripting too!"}
+}
+
+$userinput = $null
