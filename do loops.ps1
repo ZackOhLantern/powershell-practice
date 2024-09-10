52addafbea120442abfeb5fb5ea70e5eb64a6f34
ZackOhLantern <92610630+ZackOhLantern@users.noreply.github.com>
2024-09-10 15:43:32 -0700

    create do loops
    finished

---
+++ do loops.ps1
@@ -0,0 +1,19 @@
+﻿cls
+
+$i=0
+do {
+    $i++
+} while ($i -lt 5)
+echo "value of i = $i"
+
+$i=0
+while ($i -lt 5) {
+    $i++
+}
+echo "Value of i = $i"
+
+$i=0
+do {
+    $i++
+} until ($i -eq 5)
+echo "Value of i = $i"
