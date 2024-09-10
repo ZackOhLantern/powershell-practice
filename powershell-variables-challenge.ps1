85b7d43d82f6b66b215299cf288401988db212b1
ZackOhLantern <92610630+ZackOhLantern@users.noreply.github.com>
2024-09-09 21:39:20 -0700

    practicing powershell: variables challenge
    using variables and user input and writing to txt file

---
+++ powershell-variables-challenge.ps1
@@ -0,0 +1,13 @@
+﻿
+$first = read-host "enter first name"
+$last = read-host "enter last name"
+$longdate = get-date
+$date = Get-Date -format DDMMYYYY-HHMMSS
+$date += $first,$last,'.txt'
+
+$filepath = 'C:\code practice\' + $date
+
+New-Item -ItemType file -path 'C:\code practice\' -name $date
+Add-Content -Path $filepath -Value $first
+Add-Content -Path $filepath -Value $last
+Add-Content -Path $filepath -Value $longdate
