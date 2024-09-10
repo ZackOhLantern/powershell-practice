b65ea9b949c5b6d532e53cc1c939cae047d2688d
ZackOhLantern <92610630+ZackOhLantern@users.noreply.github.com>
2024-09-10 14:50:38 -0700

    Update for-loops-practice.ps1
    finished

--- for-loops-practice.ps1
+++ for-loops-practice.ps1
@@ -1,6 +1,6 @@
 ﻿cls
 
 $myarray = @("cars","trucks","motorcycles","SUVs")
-for ($i=0; $i -lt 5; $i++) {
-echo $MyArray[$i]
+for ($i=0; $i -lt $myarray.count; $i++) {
+echo ("Element $i value: " + $MyArray[$i])
 }
