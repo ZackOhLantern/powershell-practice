1693ae23b05600811fddc401b328e45e5ba579b1
ZackOhLantern <92610630+ZackOhLantern@users.noreply.github.com>
2024-09-10 15:47:35 -0700

    Create for-each-loop-practice.ps1
    finished

---
+++ for-each-loop-practice.ps1
@@ -0,0 +1,14 @@
+﻿#for-each-loop-practice
+cls
+
+#for loop
+$vehicles = @("cars","motorcycles","trucks","SUVs")
+for ($i=0; $i -lt $vehicles.count; $i++) {
+    echo ("element $i = " + $Vehicles[$i])
+}
+
+
+#for each loop
+ForEach ($Element in $vehicles) {
+    echo "Building new chassis for $element"
+}
