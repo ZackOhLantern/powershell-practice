12106784f8d0df47e6dbb05d878eb5c9978561c7
ZackOhLantern <92610630+ZackOhLantern@users.noreply.github.com>
2024-09-11 15:41:07 -0700

    Create if-then-else-switch-practice.ps1
    finished

---
+++ if-then-else-switch-practice.ps1
@@ -0,0 +1,42 @@
+﻿#practicing if-then-else-switch statements
+cls
+
+
+
+$myvar1 = 1
+$myvar2 = 2
+
+
+#if-else
+if ($myvar1 -gt $myvar2) {
+    echo "I like hotdogs" 
+} else {
+    "I dislike hotdogs" }
+
+
+
+
+$choice = read-host "please select an option (1-2)"
+
+#elseif
+if ($choice -eq 1) {
+    echo "you chose option 1"
+} elseif ($choice -eq2) {
+    echo "you chose option 2"
+} elseif ($choice -eq3) {
+    echo "you chose option 3"
+} else { 
+    echo "you did not choose a valid option"
+}
+
+
+
+# switch statement
+
+switch (read-host "please select an option (1-3)"){
+    1 {"you chose option 1"}
+    2 {"you chose option 2"}
+    3 {"you chose option 3"}
+    default {"you did not choose a valid option"}
+}
+
