4a62ed313a6c2c4c947a8156d5c118d503cb9051
ZackOhLantern <92610630+ZackOhLantern@users.noreply.github.com>
2024-09-11 17:54:30 -0700

    Create powershell-functions-practice.ps1
    finished

---
+++ powershell-functions-practice.ps1
@@ -0,0 +1,37 @@
+﻿#powershell functions
+cls
+
+Function EchoText {
+    echo "argument:" + $args
+    
+}
+
+EchoText "I am passing an argument" "this is the  2nd argument"
+
+
+Function add{
+    echo ($args[0] + $args[1])
+}
+
+Add 5 10
+
+
+
+Function EchoParameters{
+    param (
+        $firstParameter,
+        $SecondParameter
+    )
+    echo ("First: " + $firstParameter)
+    echo ("Second: " + $SecondParameter)
+}
+
+EchoParameters -SecondParameter "This is the second" -firstParameter "This is the first"
+
+
+Function EchoText2 ($FirstParameter, $secondparameter) {
+    echo ("First: " + $FirstParameter)
+    echo ("Second: " +$secondparameter)
+}
+
+Echotext2 -FirstParameter "abc" -secondparameter "xyz"
